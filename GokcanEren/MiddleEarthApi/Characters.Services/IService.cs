985b4e95baffc6830f77424fe516d36089483fed
Gökcan Eren <87297423+gokcaneren@users.noreply.github.com>
2022-05-26 14:17:22 +0300

    Service Layer added!

---
+++ GokcanEren/MiddleEarthApi/Characters.Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using Characters.DTO.Requests;
+using Characters.DTO.Responses;
+using Characters.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters.Services
+{
+    public interface IService
+    {
+         public Task<IList<CharactersRespons>> GetAll();
+        Task<CharactersRespons> GetById(int id);
+        Task<IList<CharactersRespons>> GetByName(string name);
+        Task<int> AddCharacter(AddCharacterRequest request);
+        Task UpdateCharacter(UpdateCharacterRequest request);
+        Task DeleteCharacter(int id);
+        Task<bool> IsCharacterExist(int id);
+
+    }
+}
---
+++ GokcanEren/MiddleEarthApi/Characters.Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Characters.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters.Services
+{
+    public interface IUserService
+    {
+        User Validate(string username, string password);
+    }
+}
