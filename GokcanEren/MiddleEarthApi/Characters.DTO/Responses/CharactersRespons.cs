3d75e849ac0b6c4cc49bde67440900c8642109d6
Gökcan Eren <87297423+gokcaneren@users.noreply.github.com>
2022-05-26 14:16:15 +0300

    DTO added!

---
+++ GokcanEren/MiddleEarthApi/Characters.DTO/Responses/CharactersRespons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters.DTO.Responses
+{
+    public class CharactersRespons
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int RaceId { get; set; }
+
+        public bool isAlive { get; set; } = true;
+
+        public string ImageUrl { get; set; }
+    }
+}
